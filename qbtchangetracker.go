a13af6c1307d122018795692875f484650a5d2e8
Alexey Kostin <rumanzo@yandex.ru>
2017-12-31 19:54:51 +0300

    Better error handling

--- qbtchangetracker.go
+++ qbtchangetracker.go
@@ -9,17 +9,18 @@ import (
 	"os"
 	"path/filepath"
 	"sync"
+	"time"
 )
 
 func decodetorrentfile(path string) (map[string]interface{}, error) {
 	dat, err := ioutil.ReadFile(path)
 	if err != nil {
-		log.Fatal(err)
+		log.Println(err)
 		return nil, err
 	}
 	var torrent map[string]interface{}
 	if err := bencode.DecodeBytes([]byte(dat), &torrent); err != nil {
-		log.Fatal(err)
+		log.Println(err)
 		return nil, err
 	}
 	return torrent, nil
@@ -33,14 +34,14 @@ func encodetorrentfile(path string, newstructure map[string]interface{}) error {
 
 	file, err := os.OpenFile(path, os.O_WRONLY, 0666)
 	if err != nil {
-		log.Fatal(err)
+		log.Println(err)
 		return err
 	}
 	defer file.Close()
 	bufferedWriter := bufio.NewWriter(file)
 	enc := bencode.NewEncoder(bufferedWriter)
 	if err := enc.Encode(newstructure); err != nil {
-		log.Fatal(err)
+		log.Println(err)
 		return err
 	}
 	bufferedWriter.Flush()
@@ -51,7 +52,7 @@ func changetracker(oldtracker *string, newtracker *string, path string, wg *sync
 	defer wg.Done()
 	fastresume, err := decodetorrentfile(path)
 	if err != nil {
-		log.Fatal(err)
+		log.Println(err)
 		return err
 	}
 	for num, list := range fastresume["trackers"].([]interface{}) {
@@ -60,7 +61,7 @@ func changetracker(oldtracker *string, newtracker *string, path string, wg *sync
 			fmt.Printf("Changed tracker in %v\n", path)
 			err = encodetorrentfile(path, fastresume)
 			if err != nil {
-				log.Fatal(err)
+				log.Println(err)
 				return err
 			}
 		}
@@ -71,13 +72,14 @@ func changetracker(oldtracker *string, newtracker *string, path string, wg *sync
 func main() {
 	var wg sync.WaitGroup
 	directory := os.Getenv("LOCALAPPDATA") + "\\qBittorrent\\BT_backup\\"
-	files, err := filepath.Glob(directory + "*fastresume")
-	reader := bufio.NewReader(os.Stdin)
+	files, _ := filepath.Glob(directory + "*fastresume")
+	_, err := os.Stat(directory)
 	if err != nil {
-		log.Fatal(err)
-		fmt.Print("Fatal error! Pres Enter to exit")
-		reader.ReadString('\n')
+		log.Println(err)
+		time.Sleep(30 * time.Second)
+		os.Exit(1)
 	}
+	reader := bufio.NewReader(os.Stdin)
 	fmt.Print("Enter old tracker: ")
 	oldtracker, _ := reader.ReadString('\n')
 	oldtracker = oldtracker[:len(oldtracker)-2]
@@ -89,6 +91,6 @@ func main() {
 		wg.Add(1)
 	}
 	wg.Wait()
-	fmt.Print("Pres Enter to exit")
+	fmt.Print("Press Enter to exit")
 	reader.ReadString('\n')
 }
